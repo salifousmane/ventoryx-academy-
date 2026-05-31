8c0542ddc44f1754ab0afd24e87d03767b28b4fa
Salif Ousmane Sow <salifousmanesow4@gmail.com>
2026-05-31 22:22:40 +0000

    Add main URL configuration for Django project

---
+++ urls.py
@@ -0,0 +1,31 @@
+"""
+Configuration des URLs principales.
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+]
+
+urlpatterns += i18n_patterns(
+    path('', include('apps.core.urls')),
+    path('auth/', include('apps.users.urls')),
+    path('parcours/', include('apps.parcours.urls')),
+    path('blog/', include('apps.blog.urls')),
+    path('institution/', include('apps.institution.urls')),
+    path('forum/', include('apps.forum.urls')),
+    path('messaging/', include('apps.messaging.urls')),
+    path('audit/', include('apps.audit.urls')),
+    path('gestion/', include('apps.gestion.urls')),
+)
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
