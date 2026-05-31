61d14d79a1dd4f16f14cb44afda4d988d8089ef0
Salif Ousmane Sow <salifousmanesow4@gmail.com>
2026-05-31 22:16:16 +0000

    Add ASGI configuration for Ventoryx Academy

---
+++ asgi.py
@@ -0,0 +1,9 @@
+"""
+Configuration ASGI pour Ventoryx Academy.
+"""
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+application = get_asgi_application()
